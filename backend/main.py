b7e105891d1e1164e582ef5067b7a78895e90fb7
Carina Costache <carinacostache@gmail.com>
2026-03-22 14:58:59 +0100

    feat: implemented CRUD and optimization of the route endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from routers import routes
 app= FastAPI(title="Delivery Optimizer API")
 
 
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(routes.router)
 
 @app.get("/")
 def root():
---
+++ backend/routers/routes.py
@@ -0,0 +1,72 @@
+
+
+from fastapi import APIRouter, HTTPException
+from database import routes_collection
+from models import AddRoute, serialize_route
+from services.geocoding import geocode_route
+from services.optimizer import optimize_route
+from bson import ObjectId
+from datetime import datetime
+
+router = APIRouter()
+
+#get request for list of all the routes to display on the page
+@router.get("/routes")
+def get_all_routes():
+    routes = list(routes_collection.find())
+    return [serialize_route(r) for r in routes]
+
+#post request to add a new route 
+@router.post("/routes")
+def create_route(route: AddRoute):
+    
+    #convert into a dict 
+    route_doc=route.model_dump()
+    route_doc["status"]="pending"
+    route_doc["created_at"] = datetime.now()
+
+    #call geocode_route to get the latitude and longitude
+    document=geocode_route(route_doc)
+    #insert the document into MongoDB
+    result=routes_collection.insert_one(document)
+    inserted_doc=routes_collection.find_one({"_id": result.inserted_id})
+    return serialize_route(inserted_doc)
+
+
+#get request to get a specific route
+@router.get("/routes/{id}")
+def get_route(id: str):
+    route=routes_collection.find_one({"_id": ObjectId(id)})
+    if not route:
+        raise HTTPException(status_code=404, detail="Route not found")
+    return serialize_route(route)
+
+
+#delete a specific route
+@router.delete("/routes/{id}")
+def delete_route(id: str):
+    result = routes_collection.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Route not found")
+    return {"message": "Route deleted!"}
+
+
+#post request to optimize a specific route
+@router.post("/routes/{id}/optimize")
+def get_optimized_route(id: str):
+    route= routes_collection.find_one({"_id":ObjectId(id)})
+    if not route:
+        raise HTTPException(status_code=404, detail="Route not found")
+    updated_route= optimize_route(route)
+    routes_collection.update_one(
+        {"_id": ObjectId(id)},
+        {"$set": {
+            "stops": updated_route["stops"],
+            "status": updated_route["status"],
+            "total_distance": updated_route["total_distance"],
+            "estimated_time": updated_route["estimated_time"],
+        }}
+    )
+
+    final = routes_collection.find_one({"_id": ObjectId(id)})
+    return serialize_route(final)
